00312dc44f93204ae903b0bbd9778477674c9c7c
Pedro William B Moraes <william.baiardi@hotmail.com>
2024-03-15 15:04:21 -0300

    refactor(app): Adjust responsive design and layout in app page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,36 +20,38 @@ export default function Home() {
   const typedStrings = translations[locale];
 
   return (
-    <main className="flex flex-col items-center justify-center p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <button onClick={() => switchLocale('pt-BR')} className="mx-2">PT</button>
-          | 
-          <button onClick={() => switchLocale('en-US')} className="mx-2">EN</button>
+    <main className="flex flex-col items-center justify-center p-4">
+      <div className="z-10 max-w-md sm:max-w-lg md:max-w-xl lg:max-w-5xl w-full flex flex-col lg:flex-row items-center justify-between font-mono text-xs sm:text-sm md:text-base">
+        <div className="fixed bottom-0 left-0 flex h-12 sm:h-24 md:h-36 lg:h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:relative lg:h-auto lg:w-auto lg:bg-none p-2">
+          <button onClick={() => switchLocale('pt-BR')} className="mx-1 sm:mx-2">PT</button>
+          |
+          <button onClick={() => switchLocale('en-US')} className="mx-1 sm:mx-2">EN</button>
         </div>
       </div>
 
-      <div className="relative flex flex-col items-center justify-center z-[-1]">
-        <Image
-          className="relative z-10 dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/invent-solutions.svg"
-          alt="Invent Solutions Logo"
-          width={800}
-          height={450}
-          priority
-        />
-        <div className="mt-[-60px] sm:mt-[-140px]">
+      <div className="relative flex flex-col items-center justify-center z-[-1] w-full max-w-5xl">
+        <div className="relative w-full h-0 pb-[56.25%]">
+          <Image
+            className="relative top-0 left-0 z-10 dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
+            src="/invent-solutions.svg"
+            alt="Invent Solutions Logo"
+            layout="fill"
+            objectFit="contain"
+            priority
+          />
+        </div>
+        <div className="mt-[-60px] sm:mt-[-80px] md:mt-[-100px] lg:mt-[-140px]">
           <ReactTyped
             strings={typedStrings}
             typeSpeed={40}
             backSpeed={50}
             loop
-            className="text-xl"
+            className="text-lg sm:text-xl md:text-2xl"
           />
         </div>
       </div>
 
-      <footer className="absolute bottom-0 w-full text-center p-4">
+      <footer className="absolute bottom-10 w-full text-center p-2 sm:p-4 md:p-6">
         <p>Invent Solutions - CNPJ: 17.499.859/0001-90</p>
       </footer>
     </main>
