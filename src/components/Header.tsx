aafe284ef87cf51aefb4f660b37383addef69792
Pedro William B Moraes <william.baiardi@hotmail.com>
2024-03-16 08:10:38 -0300

    refactor(locale): Update locale from 'en' to 'en-US'

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,7 +5,7 @@ export default function Header() {
     <div className="self-start mb-auto fixed top-4 left-4">
       <Link className="mx-1 sm:mx-2" href="/pt-BR">PT</Link>
         |
-      <Link  className="mx-1 sm:mx-2" href="/en">EN</Link>
+      <Link  className="mx-1 sm:mx-2" href="/en-US">EN</Link>
     </div>
   )
 }
--- src/i18n.ts
+++ src/i18n.ts
@@ -1,7 +1,7 @@
 import {notFound} from 'next/navigation';
 import {getRequestConfig} from 'next-intl/server';
  
-const locales = ['en', 'pt-BR'];
+const locales = ['en-US', 'pt-BR'];
  
 export default getRequestConfig(async ({locale}) => {
   if (!locales.includes(locale as any)) notFound();
